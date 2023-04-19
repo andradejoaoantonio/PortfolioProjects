15e890ec5dfc8af2042b633fb1ae7fa884416b2e
Frankenstein <101751046+andradejoaoantonio@users.noreply.github.com>
2023-04-19 23:43:33 +0300

    Update Data Cleaning Portfolio Project Queries.sql

--- Data Cleaning Portfolio Project Queries_final.sql
+++ Data Cleaning Portfolio Project Queries_final.sql
@@ -104,15 +104,11 @@ Update NashvilleHousing
 SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 2)
 
 
-
 ALTER TABLE NashvilleHousing
 Add OwnerSplitState Nvarchar(255);
 
 Update NashvilleHousing
 SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.') , 1)
-
-
-Select * From NashvilleHousing
 
 --------------------------------------------------------------------------------------------------------------------------
 
@@ -142,7 +138,6 @@ CASE
 	ELSE SoldAsVacant
 END
 
-
 -----------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Remove Duplicates
@@ -151,11 +146,11 @@ END
 WITH RowNumCTE AS(
 Select *, ROW_NUMBER() OVER (
 	PARTITION BY ParcelID,
-				 PropertyAddress,
-				 SalePrice,
-				 SaleDate,
-				 LegalReference
-				 ORDER BY UniqueID) row_num
+		PropertyAddress,
+		SalePrice,
+		SaleDate,
+		LegalReference
+	ORDER BY UniqueID) row_num
 From NashvilleHousing)
 Select * From RowNumCTE
 Where row_num > 1
@@ -166,66 +161,22 @@ Order by PropertyAddress
 WITH RowNumCTE AS(
 Select *, ROW_NUMBER() OVER (
 	PARTITION BY ParcelID,
-				 PropertyAddress,
-				 SalePrice,
-				 SaleDate,
-				 LegalReference
-				 ORDER BY UniqueID) row_num
+		PropertyAddress,
+		SalePrice,
+		SaleDate,
+		LegalReference
+	ORDER BY UniqueID) row_num
 From NashvilleHousing)
 DELETE From RowNumCTE
 Where row_num > 1
 
-
 ---------------------------------------------------------------------------------------------------------
 
 -- Delete Unused Columns
+-- NOTE: Be cautions when doing this because, in general, this is not a good practice to delete/change raw data, it is just for demonstration purposes only
 
 Select * From NashvilleHousing
 Select OwnerAddress, TaxDistrict, PropertyAddress, SaleDate From NashvilleHousing
 
-/*NOTE: Be aware that this is not a good practice to delete raw data, it is just for demonstration purposes only.*/
-
 ALTER TABLE NashvilleHousing
 DROP COLUMN OwnerAddress, TaxDistrict, PropertyAddress, SaleDate
-
-
------------------------------------------------------------------------------------------------
------------------------------------------------------------------------------------------------
-
---- Importing Data using OPENROWSET and BULK INSERT	
-
---  More advanced and looks cooler, but have to configure server appropriately to do correctly
---  Wanted to provide this in case you wanted to try it
-
-
---sp_configure 'show advanced options', 1;
---RECONFIGURE;
---GO
---sp_configure 'Ad Hoc Distributed Queries', 1;
---RECONFIGURE;
---GO
-
-
---USE PortfolioProject 
---GO 
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'AllowInProcess', 1 
---GO 
---EXEC master.dbo.sp_MSset_oledb_prop N'Microsoft.ACE.OLEDB.12.0', N'DynamicParameters', 1 
---GO 
-
-
----- Using BULK INSERT
---USE PortfolioProject;
---GO
---BULK INSERT nashvilleHousing FROM 'C:\Temp\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv'
---   WITH (--FIELDTERMINATOR = ',',ROWTERMINATOR = '\n');
---GO
-
-
----- Using OPENROWSET
---USE PortfolioProject;
---GO
---SELECT * INTO nashvilleHousing
---FROM OPENROWSET('Microsoft.ACE.OLEDB.12.0',
---    'Excel 12.0; Database=C:\Users\alexf\OneDrive\Documents\SQL Server Management Studio\Nashville Housing Data for Data Cleaning Project.csv', [Sheet1$]);
---GO
